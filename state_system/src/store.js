9a57aa4d248548a93cb07a6ce0b15fcd911294be
souvenir718 <mongkey0@naver.com>
2021-08-14 11:53:43 +0900

    [0814] redux방식의 store

--- state_system/src/store.js
+++ state_system/src/store.js
@@ -1,4 +1,6 @@
-import { Store } from "./core/Store.js";
+/*
+    VuexStore 사용
+import { Store } from "./core/VuexStore.js";
 
 export const store = new Store({
     state: {
@@ -19,3 +21,31 @@ export const store = new Store({
 
     // actions도 있으면 좋겠지만.. 딱히 지금 쓸만한 API가 없다.
 });
+*/
+import { createStore } from "./core/ReduxStore.js";
+
+// 초기 state의 값을 정의해준다.
+const initState = {
+    a: 10,
+    b: 20,
+};
+
+// dispatch에서 사용될 type들을 정의해준다.
+export const SET_A = "SET_A";
+export const SET_B = "SET_B";
+
+// reducer를 정의하여 store에 넘겨준다.
+export const store = createStore((state = initState, action = {}) => {
+    switch (action.type) {
+        case "SET_A":
+            return { ...state, a: action.payload };
+        case "SET_B":
+            return { ...state, b: action.payload };
+        default:
+            return state;
+    }
+});
+
+// reducer에서 사용될 action을 정의해준다.
+export const setA = (payload) => ({ type: SET_A, payload });
+export const setB = (payload) => ({ type: SET_B, payload });
